887a10ddbc62ceefc110abd9b424d5d7363df3fa
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-12-20 23:07:30 -0600

    working on layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="sidebar">
+        <div className="upperSide"></div>
+        <div className="lowerSide"></div>
+      </div>
+      <div className="main"></div>
+    </div>
+  );
 }
 
 export default App;
