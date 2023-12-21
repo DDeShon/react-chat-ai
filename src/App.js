f288e74d0a86aa5517c051fa7f948e6e47493d56
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-12-20 23:09:31 -0600

    working on layout

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ function App() {
   return (
     <div className="App">
       <div className="sidebar">
-        <div className="upperSide"></div>
+        <div className="upperSide">
+          <div className="upperSideTop">
+            <img src="" alt="" className="logo" />
+            <span className="brand">CMS Chat</span>
+          </div>
+          <button className="midBtn"></button>
+        </div>
         <div className="lowerSide"></div>
       </div>
       <div className="main"></div>
