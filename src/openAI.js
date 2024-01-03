012ad9b32170d4debfe3c75198a1f3f8ec2b608c
DDeShon <36036284+DDeShon@users.noreply.github.com>
2024-01-02 19:33:51 -0600

    working on ui layout

--- src/openAI.js
+++ src/openAI.js
@@ -1,6 +1,6 @@
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
-  apiKey: "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",
+  apiKey: apiKey,
 });
 const openai = new OpenAIApi(configuration);
 
